e903bb588c8cf5e8cc1eed0b6e210136f5c4ba5c
Abhishek Kumar <ak204479@gmail.com>
2025-06-29 19:05:34 +0530

    Logger

--- src/main/java/com/abis/project/aspect/LogginAspect.java
+++ src/main/java/com/abis/project/aspect/LogginAspect.java
@@ -2,15 +2,19 @@ package com.abis.project.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LogginAspect {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(LogginAspect.class);
+	
 	@Before("execution(public * com.abis.project.controller.PostController.getAllPost())")
 	public void log()
 	{
-		System.out.println("getAllPost Method Called!");
+		LOGGER.info("getAllPost Method Called!");
 	}
 }
